afa7d727ccbc0d32fde5d836bf93c8e02d2f3421
mahiraskin <Mahircemaskin@gmail.com>
2026-05-08 00:02:36 +0200

    World creation, neighborhood creation, fill with agents

---
+++ tests/world_test.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            ".."
+        )
+    )
+)
+
+from dynamics.world import World
+
+world = World(
+    width=20,
+    height=20,
+    num_neighborhoods=5
+)
+
+world.print_map()
